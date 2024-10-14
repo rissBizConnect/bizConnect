0469cf00d173e8a06383b1b2b0531f65b36bb333
Sonyj8658 <sda8658@gmail.com>
2024-10-14 14:09:04 +0900

    뇨뇨

--- riss/src/main/java/org/riss/bizconnect/pd/client/model/controller/ClientController.java
+++ riss/src/main/java/org/riss/bizconnect/pd/client/model/controller/ClientController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.riss.bizconnect.pd.client.model.dto.ClientDTO;
 import org.riss.bizconnect.pd.client.model.service.ClientService;
+import org.riss.bizconnect.pd.product.model.dto.ProductDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,10 +19,21 @@ public class ClientController {
 	@Autowired
 	private ClientService clientService;
 	
+	@RequestMapping("prMenu_client.do")
+	public ModelAndView clienthome(ModelAndView mv) {
+		
+		mv.setViewName("pr/prMenubar/prMenu_client");
+		ArrayList<ClientDTO> client = clientService.listAllClients();
+		
+		mv.addObject("all", client);
+		
+		return mv;
+	}
+	
 	@RequestMapping("client.do")
 	public ModelAndView clienttest(ModelAndView mv) {
 		
-		mv.setViewName("client/client");
+		mv.setViewName("pr/client/client");
 		ArrayList<ClientDTO> client = clientService.listAllClients();
 		return mv;
 	}
