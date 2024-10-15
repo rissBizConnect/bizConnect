cb403f508a433e13e901ab5d9c39783afd980fae
Sonyj8658 <sda8658@gmail.com>
2024-10-15 12:03:44 +0900

    커밋

--- riss/src/main/java/org/riss/bizconnect/hr/retire/model/service/RetireServiceImpl.java
+++ riss/src/main/java/org/riss/bizconnect/hr/retire/model/service/RetireServiceImpl.java
@@ -77,25 +77,6 @@ public class RetireServiceImpl implements RetireService {
 		
 	}
 
-	//총근무일
-	@Override
-    public int calculateTotalWorkingDays(Date userEntryDate, Date retDate) {
-        // Date를 LocalDate로 변환
-        LocalDate entryDate = userEntryDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        LocalDate retirementDate = retDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 
-        // 두 날짜 간의 일수 계산
-        return (int) ChronoUnit.DAYS.between(entryDate, retirementDate) + 1; // +1을 하여 종료일 포함
-    }
-
-    @Override
-    public void addRetire(Retire retire) {
-        // 총 근무일 계산
-        int totalWorkingDays = calculateTotalWorkingDays(retire.getUserEntryDate(), retire.getRetDate());
-        retire.setRetTotalDate(totalWorkingDays); // 총 근무일 설정
-        
-        // 퇴직자 DB에 추가하는 코드
-        retireDAO.insertTimeRetire(retire);
-    }
 	
 }
