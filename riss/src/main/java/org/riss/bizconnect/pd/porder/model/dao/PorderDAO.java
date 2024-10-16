e4b8e8a07d5c72afa0e251853395fb542642faab
Sonyj8658 <sda8658@gmail.com>
2024-10-16 14:56:28 +0900

    asdas

---
+++ riss/src/main/java/org/riss/bizconnect/pd/porder/model/dao/PorderDAO.java
@@ -0,0 +1,46 @@
+package org.riss.bizconnect.pd.porder.model.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.riss.bizconnect.pd.porder.model.dto.PorderDTO;
+import org.riss.bizconnect.pd.product.model.dto.ProductDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("porderDAO")
+public class PorderDAO {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	public ArrayList<PorderDTO> selectList() {
+		List<PorderDTO> list = sqlSessionTemplate.selectList("porderMapper.select");
+		return (ArrayList<PorderDTO>)list;
+	}
+
+	public int insertporder(PorderDTO pord) {
+		return sqlSessionTemplate.insert("porderMapper.insertporder", pord);
+	}
+	
+//	public int insertporder(PorderDTO pord) {
+//		return sqlSessionTemplate.insert("porderMapper.insertporder", pord);
+//	}
+
+	
+	
+	
+	
+	
+	/*
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
+
+	public ArrayList<PorderDTO> selectList() {
+		List<PorderDTO> list = sqlSessionTemplate.selectList("porderMapper.select");
+		return (ArrayList<PorderDTO>)list;
+	}*/
+	
+}
---
+++ riss/src/main/webapp/resources/js/hr/info.js
@@ -0,0 +1,15 @@
+function memberDelete(gid) {
+	location.href = "infoDelete.do?gId=" + gid;
+}
+
+function memberUpdate(gid) {
+	location.href = "moveInfoUpdate.do?gId=" + gid;
+}
+
+function memberRetireUpdate(gid) {
+	location.href = "memberRetireUpdate.do?gId=" + gid;
+}
+
+function test() {
+	alert('전송에 실패했습니다.');
+}
